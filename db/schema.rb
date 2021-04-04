9eba7cc791f964b2ad830098be8043bb3211e6b4
George Ho <george.h.w.ho@gmail.com>
2021-04-03 22:16:55 -0400

    Add applications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_03_213325) do
+ActiveRecord::Schema.define(version: 2021_04_04_021341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 2021_04_03_213325) do
     t.string "zip_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "applications", force: :cascade do |t|
+    t.text "description"
+    t.string "status"
+    t.bigint "applicants_id"
+    t.bigint "pets_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["applicants_id"], name: "index_applications_on_applicants_id"
+    t.index ["pets_id"], name: "index_applications_on_pets_id"
   end
 
   create_table "pets", force: :cascade do |t|
@@ -63,6 +74,8 @@ ActiveRecord::Schema.define(version: 2021_04_03_213325) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "applications", "applicants", column: "applicants_id"
+  add_foreign_key "applications", "pets", column: "pets_id"
   add_foreign_key "pets", "shelters"
   add_foreign_key "veterinarians", "veterinary_offices"
 end
