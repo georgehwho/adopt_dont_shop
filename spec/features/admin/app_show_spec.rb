8742698a35c896baec2315d84e807851bdeb6c98
George Ho <george.h.w.ho@gmail.com>
2021-04-07 10:57:54 -0400

    Add accept and reject conditional admin apps

--- spec/features/admin/app_show_spec.rb
+++ spec/features/admin/app_show_spec.rb
@@ -44,4 +44,35 @@ describe 'admin application show page' do
     expect(page).to have_content("Rejected")
     expect(page).to_not have_button("Reject this Pet")
   end
+
+  describe 'updates status once all pet_apps are checked' do
+    it 'has status as accepted if all apps are accepted' do
+      visit "admin/applications/#{@app_1.id}"
+
+      expect(page).to have_content("Status: Pending")
+
+      click_on("Adopt this Pet")
+
+      expect(page).to have_content("Status: Accepted")
+    end
+
+    it 'has status as rejected if any apps are rejected' do
+      pet_2 = create(:random_pet, adoptable: true)
+      @app_1.pets << pet_2
+
+      visit "admin/applications/#{@app_1.id}"
+
+      expect(page).to have_content("Status: Pending")
+
+      within "#pet-#{@pet.id}" do
+        click_on("Adopt this Pet")
+      end
+
+      expect(page).to have_content("Status: Pending")
+
+      click_on("Reject this Pet")
+
+      expect(page).to have_content("Status: Rejected")
+    end
+  end
 end
