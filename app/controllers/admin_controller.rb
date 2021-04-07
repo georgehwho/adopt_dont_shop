3f886e09ac6e2f3d7d1e8cd2c0c22c7f709032d2
George Ho <george.h.w.ho@gmail.com>
2021-04-07 08:44:23 -0400

    Add admin application accept

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,21 +7,4 @@ class AdminController < ApplicationController
   def app_show
     @application = Application.find(params[:app_id])
   end
-
-  def app_update
-    application = Application.find(params[:app_id])
-
-    if application.update(application_params)
-      redirect_to admin_app_path(application)
-    else
-      redirect_to admin_app_path(application)
-      flash[:alert] = "Error: #{error_message(application.errors)}"
-    end
-  end
-
-  private
-
-  def application_params
-    params.permit(:id, :name, :address, :city, :state, :zip_code, :description, :status)
-  end
 end
