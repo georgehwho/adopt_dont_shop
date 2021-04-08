14ccaa8e57cd7f0a95bff394ab236bfdca9fad29
George Ho <george.h.w.ho@gmail.com>
2021-04-08 13:23:36 -0400

    Update pending to order by name

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -2,7 +2,7 @@ module Admin
   class SheltersController < ApplicationController
     def index
       @shelters = Shelter.order_by_name_reverse
-      @pending = Shelter.with_pending_applications
+      @pending = Shelter.order_by_name.with_pending_applications
     end
 
     def show
