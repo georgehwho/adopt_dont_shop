373f845ba4bf2ee24c3121c26e1e372a2ebdf3f6
George Ho <george.h.w.ho@gmail.com>
2021-04-08 13:22:58 -0400

    Remove commented code

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,6 @@ Rails.application.routes.draw do
   post '/pet_applications', to: 'pet_applications#create'
   patch '/pet_applications', to: 'pet_applications#update'
 
-  # get '/admin/shelters', to: 'admin#shelters'
-  # get '/admin/applications/:app_id', to: 'admin#app_show', as: 'admin_app'
-
   namespace :admin do
     resources :shelters, only: [:index, :show]
     resources :applications, only: [:show]
